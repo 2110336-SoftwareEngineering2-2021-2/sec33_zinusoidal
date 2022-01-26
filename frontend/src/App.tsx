aff0a662a523573a0189c829c5c22c8d3b5ed5a6
Chaiwat Chaiyapuekcharoen <chaiwat.cyp@gmail.com>
2022-01-26 21:56:19 +0700

    reinit project

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,38 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
-
+import Block from "./components/Block";
 function App() {
-  return <div className="App">aasdasdasd</div>;
+  const [number, setNumber] = useState(0);
+
+  const increment = () => {
+    setNumber((prevNumber) => prevNumber + 1);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "green",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      This page is rendered {number} times
+      <Block>
+        This is Landing Page of fortune168
+        <></>
+      </Block>
+      <button
+        style={{ width: 120, height: 20, marginTop: 20 }}
+        type="button"
+        onClick={increment}
+      >
+        Click to rerender
+      </button>
+    </div>
+  );
 }
 
 export default App;
