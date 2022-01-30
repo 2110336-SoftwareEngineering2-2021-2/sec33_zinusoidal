a005aa32281c96b72077f337117cce5bbea4ed9a
snowyii <85404386+snowyii@users.noreply.github.com>
2022-01-30 21:13:17 +0700

    Revert "Create Login Form"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";
 import Register from "./pages/Register";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 const App = () => {
@@ -11,8 +10,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<LoginPage />} />
-
+          <Route path="/login" element={<></>} />
         </Routes>
       </BrowserRouter>
     </>
