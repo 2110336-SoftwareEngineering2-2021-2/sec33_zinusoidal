34771fd49652a6df5b05929511c6a4e980a63546
Mekkathon <mattanyu@gmail.com>
2022-02-02 19:10:27 +0700

    edit the database schema

---
+++ database/db_cus_reg.sql
@@ -0,0 +1,5 @@
+INSERT INTO `fortune_user`(`id`,`username`,`citizen_id`,`email`,`password`)
+    VALUES (`id`,`username`,`citizen_id`,`email`,`password`)
+
+INSERT INTO `customer`(`id`,`first_name`,`last_name`,`profile_image`)
+    VALUES (`id`,`first_name`,`last_name`,`profile_image`)
