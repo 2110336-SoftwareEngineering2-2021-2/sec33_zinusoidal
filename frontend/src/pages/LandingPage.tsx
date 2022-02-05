cb0f485cb9984588cbb7320e6201c573b3a8363a
Chaiwat Chaiyapuekcharoen <chaiwat.cyp@gmail.com>
2022-02-05 22:49:58 +0700

    :umbrella: fix aspect ratio

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -26,17 +26,20 @@ const Layout = styled.div`
 
 const Content = styled.div`
   margin: 5rem 3rem 0rem;
+  /* margin: 5rem 129px 3rem; */
   width: auto;
   background-image: url(${img1});
 
   background-repeat: no-repeat;
   background-size: 100% 100%;
-  padding-top: 40%;
+  padding-top: 50%;
   position: relative;
 `;
 
 const Content2 = styled.div`
   margin: 0rem 0rem;
+  /* margin: 0rem 129px; */
+
   padding-left: 3rem;
   padding-right: 3rem;
 
@@ -45,7 +48,7 @@ const Content2 = styled.div`
 
   background-repeat: no-repeat;
   background-size: 100% 100%;
-  padding-top: 40%;
+  padding-top: 50%;
 `;
 
 const Content3 = styled.div`
