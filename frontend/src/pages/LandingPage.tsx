da3c315cc86da0b9a17767a6ba76c177d7379677
Chaiwat Chaiyapuekcharoen <chaiwat.cyp@gmail.com>
2022-02-06 17:07:57 +0700

    :rainbow: fix landingBg3

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -11,7 +11,10 @@ const LandingPage = () => {
       <LandingNav></LandingNav>
       <Content></Content>
       <Content2></Content2>
-      <Content3></Content3>
+      <Content3>
+        <div style={{ flex: 1 }}></div>
+        <Content3in></Content3in>
+      </Content3>
     </Layout>
   );
 };
@@ -19,13 +22,12 @@ const LandingPage = () => {
 const Layout = styled.div`
   width: 100%;
   height: 100%;
-  /* background-color: red; */
   display: flex;
   flex-direction: column;
 `;
 
 const Content = styled.div`
-  margin: 5rem 3rem 0rem;
+  margin: 1rem 3rem 0rem;
   /* margin: 5rem 129px 3rem; */
   width: auto;
   background-image: url(${img1});
@@ -49,17 +51,27 @@ const Content2 = styled.div`
   background-repeat: no-repeat;
   background-size: 100% 100%;
   padding-top: 50%;
+  position: relative;
 `;
 
 const Content3 = styled.div`
-  margin: 5rem 3rem 0rem;
+  margin: 0rem 3rem 0rem;
 
   width: auto;
-  background-image: url(${img3});
+  /* background-image: url(${img3}); */
 
   background-repeat: no-repeat;
-  background-size: 100% 100%;
+
+  display: flex;
+`;
+
+const Content3in = styled.div`
   padding-top: 40%;
+  width: auto;
+  background-image: url(${img3});
+  flex: 1;
+  background-size: 100% 100%;
+  /* background-color: red; */
 `;
 
 export default LandingPage;
