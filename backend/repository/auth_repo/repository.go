0def16fae790e5f6f29963dc0fa19f0c0b41b6ac
Supakorn <supakornmanza21@gmail.com>
2022-02-08 21:57:35 +0700

    erase comment

--- backend/repository/auth_repo/repository.go
+++ backend/repository/auth_repo/repository.go
@@ -15,16 +15,11 @@ func New(db *gorm.DB) *GromDB {
 }
 
 func (db *GromDB) RegisterCustomer(customer model.Customer) error {
-	/**
-		SQL script
-		INSERT INTO `fortune_user`(`id`,`username`,`citizen_id`,`email`,`password`,`create_datetime`)
-	    VALUES (@id,@username,@citizen_id,@email,@password,NOW());
-		INSERT INTO `customer`(`id`,`first_name`,`last_name`,`profile_image`)
-	    VALUES (@id,@first_name,@last_name,@profile_image);
-	*/
 	insert_user := `INSERT INTO fortune_user(id,username,citizen_id,email,password,user_type, create_datetime)
     VALUES (?, ? , ? , ? ,?, ? ,NOW());`
-
+	/**
+	* user_type = 0 for customer
+	 */
 	err := db.database.Exec(insert_user, customer.UserId, customer.Username,
 		customer.CitizenId, customer.Email, customer.Password, false).Error
 	if err != nil {
@@ -38,23 +33,14 @@ func (db *GromDB) RegisterCustomer(customer model.Customer) error {
 }
 
 func (db *GromDB) RegisterProvider(provider model.Provider) error {
-	/**
-		SQL script
-		INSERT INTO `fortune_user`(`id`,`username`,`citizen_id`,`email`,`password`,`create_datetime`)
-	    VALUES (@id,@username,@citizen_id,@email,@password,NOW());
-		INSERT INTO `provider`(`id`,`first_name`,`last_name`,`profile_image`,`biography`,`schedule`)
-	    VALUES (@id,@first_name,@last_name,@profile_image,@biography,@schedule);
-		 for all service, one-by-one
-		INSERT INTO `provider_service`(`provider_id`,`fortune_type`,`price`)
-		VALUES (@provider_id,@fortune_type,@price);
-	*/
+
 	insert_user := `INSERT INTO fortune_user(id,username,citizen_id,email,password, user_type, create_datetime)
     VALUES (?, ? , ? , ? ,?, ? ,NOW());`
 	err := db.database.Exec(insert_user, provider.UserId, provider.Username,
 		provider.CitizenId, provider.Email, provider.Password, true).Error
 	/**
-	user type = 1
-	*/
+	* user type = 1, for provider
+	 */
 	if err != nil {
 		return err
 	}
@@ -80,12 +66,6 @@ func (db *GromDB) RegisterProvider(provider model.Provider) error {
 }
 
 func (db *GromDB) Login(username, password string) (string, error) {
-	/**
-		SQL script
-			SELECT U.id
-	    	FROM `fortune_user` U
-	    	WHERE U.`username` = @username AND U.`password` = @password;
-	*/
 
 	login_command := `SELECT U.id, U.password
 	FROM fortune_user U
@@ -108,26 +88,12 @@ func (db *GromDB) Login(username, password string) (string, error) {
 }
 
 func (db *GromDB) InsertConfirmationKey(userId, key string) error {
-	/**
-		INSERT INTO `activation_key`(`id`,`activation_key`)
-	    VALUES (@id,@activation_key);
-	*/
 	insert_key := `INSERT INTO activation_key(id, activation_key)
 	VALUES (? , ?);`
 	return db.database.Exec(insert_key, userId, key).Error
 }
 
 func (db *GromDB) ConfirmEmail(key string) error {
-	/**
-		UPDATE `fortune_user`
-	    SET `email_confirmed` = 1
-	    WHERE EXISTS
-	    (
-	        SELECT 1
-	        FROM `activation_key`
-	        WHERE `fortune_user`.`id` = `activation_key`.`id` AND `activation_key`.`activation_key` = @key
-	    );
-	*/
 	confirmEmailCommand := `UPDATE fortune_user
     SET email_confirmed = 1
     WHERE EXISTS
@@ -139,9 +105,3 @@ func (db *GromDB) ConfirmEmail(key string) error {
 
 	return db.database.Raw(confirmEmailCommand, key).Error
 }
-
-/* unused
-func (db *GromDB) IsExistUsernameAndEmail(username, email string) error {
-	return nil
-}
-*/
