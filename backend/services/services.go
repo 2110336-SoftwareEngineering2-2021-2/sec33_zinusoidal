7694d8de7294669ea447ba5951c8cf9e22a1e19b
KirkPig <KirkPig@github.com>
2022-02-15 23:19:56 +0700

    ✨ add upload image and register upload image

--- backend/services/services.go
+++ backend/services/services.go
@@ -1,1 +1,46 @@
 package services
+
+import (
+	"fmt"
+	"mime/multipart"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"github.com/spf13/viper"
+)
+
+type Service struct {
+	awsSession *session.Session
+}
+
+func NewService(sess *session.Session) *Service {
+	return &Service{
+		awsSession: sess,
+	}
+}
+
+func (s *Service) UploadFile(file multipart.FileHeader) (string, error) {
+	uploader := s3manager.NewUploader(s.awsSession)
+	blobFile, err := file.Open()
+	bucketName := viper.GetString("bucket.name")
+	bucketZone := viper.GetString("bucket.zone")
+
+	if err != nil {
+		return "", fmt.Errorf("File corrupt")
+	}
+
+	_, err = uploader.Upload(&s3manager.UploadInput{
+		Bucket: aws.String(bucketName),
+		ACL:    aws.String("public-read"),
+		Key:    aws.String(file.Filename),
+		Body:   blobFile,
+	})
+	if err != nil {
+		return "", fmt.Errorf("Upload error")
+	}
+
+	filepath := "https://" + bucketName + "." + "s3-" + bucketZone + ".amazonaws.com/" + file.Filename
+
+	return filepath, nil
+}
