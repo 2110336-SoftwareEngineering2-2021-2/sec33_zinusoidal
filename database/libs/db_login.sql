fefa2a43cea065efe952b02975b2e1993a6009d9
Mekkathon <mattanyu@gmail.com>
2022-02-15 20:19:56 +0700

    change login script

--- database/libs/db_login.sql
+++ database/libs/db_login.sql
@@ -1,3 +1,7 @@
-SELECT U.id
-    FROM fortune_user U
+SELECT U.id, U.username, C.first_name, C.last_name, C.profile_image 
+    FROM fortune_user U RIGHT JOIN customer C ON U.id = C.id
+    WHERE U.username = @username AND U.password = @password
+UNION
+SELECT U.id, U.username, P.first_name, P.last_name, P.profile_image 
+    FROM fortune_user U RIGHT JOIN provider P ON U.id = P.id
     WHERE U.username = @username AND U.password = @password;
