17f1bd0ab863752bb02f8d9f468a6b5f6c066fc1
Mekkathon <mattanyu@gmail.com>
2022-02-23 13:09:44 +0700

    change appointment table structure

--- database/libs/db_make_appointment.sql
+++ database/libs/db_make_appointment.sql
@@ -1,2 +1,13 @@
-INSERT INTO appointment(appointment_id,customer_id,provider_id,fortune_type,price,information,start_time,finish_time)
-    VALUES (@appointment_id,@customer_id,@provider_id,@fortune_type,@price,@information,@start_time,@finish_time);
+INSERT INTO appointment(appointment_id,customer_id,provider_id)
+    VALUES (@appointment_id,@customer_id,@provider_id);
+
+INSERT INTO appointment_service(appointment_id,fortune_type,price,start_time,finish_time)
+    VALUES @id+fortune_type+price_+startfinishtime_list;
+
+INSERT INTO appointment_info(appointment_id,info_name,info_value)
+    VALUES @id+nam+valu;
+
+/* example
+@id+fortune_type+price_+startfinishtime_list = ("idaa","fxx",50,"2022-02-23 05:00:00","2022-02-23 06:00:00"),("idaa","fyy",99,"2022-02-22 22:00:00","2022-02-22 22:30:00")
+@id+nam+valu = ("idaa","phone","088-888-8888"),("idaa","car license plate","AE 0712")
+*/
--- database/migrations/010_db_init_appointment.up.sql
+++ database/migrations/010_db_init_appointment.up.sql
@@ -4,12 +4,6 @@ CREATE TABLE appointment
     appointment_id varchar(100),
 	customer_id varchar(100) NOT NULL,
     provider_id varchar(100) NOT NULL,
-	fortune_type varchar(100),
-    information varchar(500),
-    
-    price int,
-    start_time datetime NOT NULL,
-    finish_time datetime NOT NULL,
     status BIT(2) default 0,   /* 0 pending, 1 rejected, 2 accepted */
 
     PRIMARY KEY (appointment_id),
