649b18500af59721de51ef3a3d58e34941655cc7
Mekkathon <mattanyu@gmail.com>
2022-02-24 20:06:31 +0700

    fix db

--- database/migrations/012_db_init_appointment_info.up.sql
+++ database/migrations/012_db_init_appointment_info.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE appointment_info
 (
     appointment_id varchar(100),
 	info_name varchar(100),
-    info_value varchar(500)
+    info_value varchar(500),
 
     PRIMARY KEY (appointment_id,info_name)
 );
