07b34a1ea12b7ac7bcd70fd268f7204dfd92a677
Supakorn <supakornmanza21@gmail.com>
2022-02-25 22:39:54 +0700

    fixed cors

--- backend/main.go
+++ backend/main.go
@@ -45,7 +45,7 @@ func main() {
 	config := cors.DefaultConfig()
 	config.AllowOriginFunc = func(origin string) bool { return true }
 	config.AllowOrigins = []string{"*"}
-	config.AllowAllOrigins = true
+	//config.AllowAllOrigins = true
 	config.AllowHeaders = []string{"Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With"}
 	//config.AllowHeaders = []string{"*"}
 
