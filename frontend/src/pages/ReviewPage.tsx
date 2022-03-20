e85003fbe3fee01475800fc1bfa3b5eacfaabc6c
Chaiwat Chaiyapuekcharoen <chaiwat.cyp@gmail.com>
2022-03-20 17:26:06 +0700

    :star: finish rating page

---
+++ frontend/src/pages/ReviewPage.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import CreateReviewModal from "../components/review/CreateReviewModal";
+const ReviewPage = () => {
+  return (
+    <Layout>
+      <CreateReviewModal />
+    </Layout>
+  );
+};
+
+const Layout = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-image: linear-gradient(
+    to left top,
+    #d898fd,
+    #dca5fc,
+    #e0b2fb,
+    #e3bffa,
+    #e7cbf8,
+    #e7cbf8,
+    #e7cbf8,
+    #e7cbf8,
+    #e3bffa,
+    #e0b2fb,
+    #dca5fc,
+    #d898fd
+  );
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+export default ReviewPage;
