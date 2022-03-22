5d51cc028888c6e8c1bf26613eebcba59d80d58b
Sarawut25360 <bawhk1412@gmail.com>
2022-03-22 21:01:38 +0700

    block commit 1

---
+++ frontend/src/components/chat/ChatBlock.tsx
@@ -0,0 +1,7 @@
+import React, { useState, useEffect, useRef } from "react";
+import styled from "styled-components";
+
+const Chat = ({ chatMessage, selectedUser, setMessage, message }: any) => {
+  return null;
+};
+export default Chat;
