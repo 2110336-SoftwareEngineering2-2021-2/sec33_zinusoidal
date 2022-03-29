57236758a0f71619f1ba76a27f63f45ccb82b6bd
Kirk Pig <kriklomark@gmail.com>
2022-03-29 20:12:41 +0700

    Update repository.go

--- backend/repository/review_repo/repository.go
+++ backend/repository/review_repo/repository.go
@@ -14,7 +14,7 @@ func New(db *gorm.DB) *GromDB {
 
 func (db *GromDB) InsertReview(appointmentId string, score int, text string) error {
 
-	query := `INSERT INTO review(appointment_id,review_score,review_comment)
+	query := `INSERT INTO review(appointment_id,score,comment)
     VALUES(?,?,?);`
 
 	return db.database.Exec(query, appointmentId, score, text).Error
